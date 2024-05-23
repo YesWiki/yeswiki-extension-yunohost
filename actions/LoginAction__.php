6d577f5255f441b7b44a498265c3e4d76e4e10d0
mrflos <mrflos@develop.lab12.io>
2024-05-23 21:12:52 +0200

    feat(sso): make redirection to wiki after sso login/logout

--- actions/LoginAction__.php
+++ actions/LoginAction__.php
@@ -9,16 +9,19 @@ class LoginAction__ extends YesWikiAction
 {
     public function run()
     {
+        $incomingUrl = (empty($_SERVER['HTTPS']) ? 'http' : 'https').
+            "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $encodedUrl = base64_encode($incomingUrl);
         $authController = $this->getService(AuthController::class);
         if ($authController->getLoggedUser()) {
             $this->output = preg_replace(
                 '~"https?://.*action=logout"~Ui',
-                '/yunohost/sso/?action=logout',
+                '/yunohost/sso/?action=logout&r='.$encodedUrl,
                 $this->output
             );
         } else {
-            $this->output = '<a href="/yunohost/sso/" class="btn btn-default btn-default ">
-        <i class="glyphicon glyphicon-user"></i> Se connecter</a>';
+            $this->output = '<a href="/yunohost/sso/?r='.$encodedUrl.'" class="btn btn-default btn-default ">
+        <i class="glyphicon glyphicon-user"></i> '._t('LOGIN_LOGIN').'</a>';
         }
     }
 }
