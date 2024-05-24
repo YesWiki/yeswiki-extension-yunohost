64154675b455b52d78dca17f197a36d9da808e83
mrflos <mrflos@chmok.net>
2024-05-24 11:49:41 +0200

    feat(sso): add config parameter and run from local script

--- actions/LoginAction__.php
+++ actions/LoginAction__.php
@@ -9,19 +9,22 @@ class LoginAction__ extends YesWikiAction
 {
     public function run()
     {
-        $incomingUrl = (empty($_SERVER['HTTPS']) ? 'http' : 'https').
-            "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        $encodedUrl = base64_encode($incomingUrl);
-        $authController = $this->getService(AuthController::class);
-        if ($authController->getLoggedUser()) {
-            $this->output = preg_replace(
-                '~"https?://.*action=logout"~Ui',
-                '/yunohost/sso/?action=logout&r='.$encodedUrl,
-                $this->output
-            );
-        } else {
-            $this->output = '<a href="/yunohost/sso/?r='.$encodedUrl.'" class="btn btn-default btn-default ">
-        <i class="glyphicon glyphicon-user"></i> '._t('LOGIN_LOGIN').'</a>';
+        if ($this->params->has('enable_yunohost_sso') &&
+            $this->params->get('enable_yunohost_sso')) {
+            $incomingUrl = (empty($_SERVER['HTTPS']) ? 'http' : 'https').
+                "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+            $encodedUrl = base64_encode($incomingUrl);
+            $authController = $this->getService(AuthController::class);
+            if ($authController->getLoggedUser()) {
+                $this->output = preg_replace(
+                    '~"https?://.*action=logout"~Ui',
+                    '/yunohost/sso/?action=logout&r='.$encodedUrl,
+                    $this->output
+                );
+            } else {
+                $this->output = '<a href="/yunohost/sso/?r='.$encodedUrl.'" class="btn btn-default btn-default ">
+            <i class="glyphicon glyphicon-user"></i> '._t('LOGIN_LOGIN').'</a>';
+            }
         }
     }
 }
--- actions/__LoginAction.php
+++ actions/__LoginAction.php
@@ -10,48 +10,54 @@ class __LoginAction extends YesWikiAction
 {
     public function run()
     {
-        $authController = $this->getService(AuthController::class);
-        $userManager = $this->getService(UserManager::class);
 
-        if (empty($_SERVER['REMOTE_USER'])) {
-            // disconnect any connected user
-            $authController->logout();
-        } else {
-            $needLogin = true;
-            if ($user = $authController->getLoggedUser()) {
-                if ($user['name'] == $_SERVER['REMOTE_USER']) {
-                    // good user is already connected, nothing to do
-                    $needLogin = false;
-                } else {
-                    // we logout already logged user that doesn't match
-                    $authController->logout();
-                }
-            }
-            if ($needLogin) {
-                $user = $userManager->getOneByName($_SERVER['REMOTE_USER']);
-                if (!$user) {
-                    // user needs to be created
-                    $output = null;
-                    $retval = null;
-                    exec('sudo /usr/local/bin/yunohost-user-info.sh ' . escapeshellcmd($_SERVER['REMOTE_USER']) . ' --output-as json', $output, $retval);
-
-                    if ($retval == 0) {
-                        $email = json_decode($output[0])->mail;
+        if ($this->params->has('enable_yunohost_sso') &&
+            $this->params->get('enable_yunohost_sso')) {
+            $authController = $this->getService(AuthController::class);
+            $userManager = $this->getService(UserManager::class);
+            $_SERVER['REMOTE_USER'] = 'toto';
+            if (empty($_SERVER['REMOTE_USER'])) {
+                // disconnect any connected user
+                $authController->logout();
+            } else {
+                $needLogin = true;
+                if ($user = $authController->getLoggedUser()) {
+                    if ($user['name'] == $_SERVER['REMOTE_USER']) {
+                        // good user is already connected, nothing to do
+                        $needLogin = false;
                     } else {
-                        exit('yunohost user info returned an error!');
+                        // we logout already logged user that doesn't match
+                        $authController->logout();
+                    }
+                }
+                if ($needLogin) {
+                    $user = $userManager->getOneByName($_SERVER['REMOTE_USER']);
+                    if (!$user) {
+                        // user needs to be created
+                        $output = null;
+                        $retval = null;
+
+                        exec('sudo -n '.getcwd().'/tools/yunohost/private/scripts/yunohost-user-info.sh ' . escapeshellcmd($_SERVER['REMOTE_USER']) . ' --output-as json 2>&1', $output, $retval);
+
+                        if ($retval == 0) {
+                            $email = json_decode($output[0])->mail;
+                        } else {
+                            exit('yunohost-user-info.sh returned an error:<br>'.implode('<br>', $output));
+                        }
+
+                        // add to local database
+                        $userManager->create($_SERVER['REMOTE_USER'], $email, 'Password handled by YunoHost SSO');
+                        // get the user's info for login
+                        $user = $userManager->getOneByName($_SERVER['REMOTE_USER']);
                     }
 
-                    // add to local database
-                    $userManager->create($_SERVER['REMOTE_USER'], $email, 'Password handled by YunoHost SSO');
-                    // get the user's info for login
-                    $user = $userManager->getOneByName($_SERVER['REMOTE_USER']);
-                }
-
-                // login user
-                if ($user) {
-                    $authController->login($user);
+                    // login user
+                    if ($user) {
+                        $authController->login($user);
+                    }
                 }
             }
         }
+
     }
 }
