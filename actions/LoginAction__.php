471e748955c468768589213212992e2dc72d7184
Douze Baie <12b@distrilab.fr>
2024-06-14 14:00:56 +0200

    fix(sso): use yunohost_sso_domain config parameter for SSO URL

--- actions/LoginAction__.php
+++ actions/LoginAction__.php
@@ -10,6 +10,7 @@ class LoginAction__ extends YesWikiAction
     public function run()
     {
         if ($this->params->has('enable_yunohost_sso') &&
+            $this->params->has('yunohost_sso_domain') &&
             $this->params->get('enable_yunohost_sso')) {
             $incomingUrl = (empty($_SERVER['HTTPS']) ? 'http' : 'https').
                 "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
@@ -18,11 +19,11 @@ class LoginAction__ extends YesWikiAction
             if ($authController->getLoggedUser()) {
                 $this->output = preg_replace(
                     '~"https?://.*action=logout"~Ui',
-                    '/yunohost/sso/?action=logout&r='.$encodedUrl,
+                    'https://'.$this->params->get('yunohost_sso_domain').'/yunohost/sso/?action=logout&r='.$encodedUrl,
                     $this->output
                 );
             } else {
-                $this->output = '<a href="/yunohost/sso/?r='.$encodedUrl.'" class="btn btn-default btn-default ">
+                $this->output = '<a href="https://'.$this->params->get('yunohost_sso_domain').'/yunohost/sso/?r='.$encodedUrl.'" class="btn btn-default btn->
             <i class="glyphicon glyphicon-user"></i> '._t('LOGIN_LOGIN').'</a>';
             }
         }
