78a2e95cffeea28e0655ba116177d047069e06ed
Florian Schmitt <mrflos@gmail.com>
2025-01-28 12:33:15 +0300

    feat(ssologin): backward compatibility with YunoHost 11

--- actions/LoginAction__.php
+++ actions/LoginAction__.php
@@ -15,17 +15,32 @@ class LoginAction__ extends YesWikiAction
             $incomingUrl = (empty($_SERVER['HTTPS']) ? 'http' : 'https').
                 "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
             $encodedUrl = base64_encode($incomingUrl);
+            if (!file_exists('files/yunohost_version')) {
+                exec('dpkg-query --show --showformat=\'${Version}\' yunohost > files/yunohost_version');
+            }
+            $yunohostVersion = file_get_contents('files/yunohost_version');
+            $majorYunohostVersion = explode('.', $yunohostVersion)[0] ?? '';
+            // we default on current stable Yunohost's version urls
+            $loginUrl = 'https://'.$this->params->get('yunohost_sso_domain').'/yunohost/sso/login?r='.$encodedUrl;
+            $logoutUrl = 'https://'.$this->params->get('yunohost_sso_domain').'/yunohost/portalapi/logout?referer_redirect';
+;
+            if ($majorYunohostVersion == '11') { 
+                // backward compatibility
+                $loginUrl = 'https://'.$this->params->get('yunohost_sso_domain').'/yunohost/sso/?r='.$encodedUrl;
+                $logoutUrl = 'https://'.$this->params->get('yunohost_sso_domain').'/yunohost/sso/?action=logout&r='.$encodedUrl;
+            }
+
             $authController = $this->getService(AuthController::class);
             if ($authController->getLoggedUser()) {
                 $this->output = preg_replace(
                     '~"https?://.*action=logout"~Ui',
-                    'https://'.$this->params->get('yunohost_sso_domain').'/yunohost/portalapi/logout?referer_redirect',
+                    $logoutUrl,
                     $this->output
                 );
             } else {
-                $this->output = '<a href="https://'.$this->params->get('yunohost_sso_domain').'/yunohost/sso/login?r='.$encodedUrl.'" class="btn btn-default btn->
+                $this->output = '<a href="''.$loginUrl.'" class="btn btn-default btn->
             <i class="glyphicon glyphicon-user"></i> '._t('LOGIN_LOGIN').'</a>';
             }
         }
     }
-} 
+}
